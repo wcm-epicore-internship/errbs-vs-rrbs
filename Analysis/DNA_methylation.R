28035da07bfb795a59ad3d4a259ff25abdd1ba0e
ShazedaOmar98 <36874225+ShazedaOmar98@users.noreply.github.com>
2018-10-09 13:03:02 -0400

    Update DNA_methylation.R

--- Analysis/DNA_methylation.R
+++ Analysis/DNA_methylation.R
@@ -2,81 +2,88 @@
 library(methylKit)
 library(GenomicRanges)
 
-#read in file 
-file.names.list <- list.files(path = "/Users/shazedaomar/Desktop/Internship_notes/MethylCall_Data_Epicore", pattern = "*.mincov0.txt.gz", full.names = TRUE)
-head(file.names.list)
+compressed.files <- FALSE
 
-#methread for MULTIPLE FILES
-#read the files to a methylRawListDB object: myobjDB
-# save the obj so that it doesn't have to be run everytime??  
-myobj=methRead(as.list(file.names.list),
-sample.id=as.list(gsub(".mincov0.txt.gz", "", basename(file.names.list))), #gsub finds all the files that end in ("*.mincov0.txt.gz"), Basename removes the ending of the file.
-assembly="hg19",
-treatment=rep(0,6), #rep function - 0 = same , 6 = number of items in the file.names.list
-context="CpG",
-dbdir = "methylDB"
-)
+#read in files
+#store path into variable data.file.path
+#data.file.path <- "/Users/shazedaomar/Desktop/Internship_notes/MethylCall_Data_Epicore"
+data <- data.file.path
+file.names.list <- list.files(path = data, pattern = "*.mincov0.txt.gz", full.names = TRUE)
+file.names.list
 
-#creates a path to the folder methylDB -- compressed and indexed files
-print(myobj[[1]]@dbpath) #need to run the dbtype = "tabix", command to get result
+#methread for Multiple Files
+#save it so that it doesn't have to be run everytime - saves more time
+rda <- paste(rdataDir, "mcgrlist.rda", sep="/")
+cat("saving mcgrlist to [", rda, "]\n")
+save(mcgrlist, file=rda)
+# but then you will have to load it
+
+if  (!exists("myobj")) {
+    cat("Did not find myobj\n")
+    elseif(file.exists("myobj.rda")) {
+        load("myobj.rda\n")
+        cat("myobj.rda loaded")
+   else { 
+        cat("creating and saving myobj\n")
+  myobj=methRead(as.list(file.names.list),
+        sample.id=as.list(gsub(".mincov0.txt.gz", "", basename(file.names.list))), #gsub finds all the files that end in ("*.mincov0.txt.gz"), Basename removes the ending of the file.
+        assembly="hg19",
+        treatment=rep(0,6), #rep function - 0 = same , 6 = number of items in the file.names.list
+        context="CpG",
+        dbdir = "methylDB"
+        )
+        save(myobj,file = "myobj.rda")
+        cat ("myobj saved \n")
+   }
+        }
+} 
 
 
-#Output the file names and nrows for each sample  
-samples <- sapply(myobj,nrow) 
-names(samples) <- c("ERRBS", "methylcaptureA", "methylcaptureB", "methylcaptureC", "RRBS", "WGBS")
+#creates a path to the folder methylDB -- compressed and indexed files .tbi and .bgz
+#print(myobj[[1]]@dbpath) #need to run the dbtype = "tabix", command to get result
+
+#Output the file names and nrows for each sample
+samples <- sapply(myobj,nrow)
+names(samples) <- getSampleID(myobj)
 file.list <- as.list(samples)
 file.list
 
+#myobj[[i]]@sample.id
 
-#using a function to find nrow, statistics and histogram (condense code)
-methylation <- function(myobj, files.names.lists) {
-    
-    for (i in 1:length(file.names.list)) {
-        print(basename(file.names.list[[i]]))
-        print(nrow(myobj[[i]]))
+#Methylation function:Counts the number of rows, descriptive statistics, percent methylation distribution, histogram with the percentage of CpG for each dataset.
+methylation <- function(myobj) {
+    for (i in 1:length(myobj)) {
+        
+        sample.output <- myobj[[i]]@sample.id
+        cat("sample", sample.output, "\n", sep=": ")
+        cat("number of sites", nrow(myobj[[i]]), "\n", sep=": ")
+        pdf(paste(sample.output,"methylation_output.pdf",sep="_"))
         getMethylationStats(myobj[[i]])
         getMethylationStats(myobj[[i]],plot=TRUE,both.strands=FALSE)
         getCoverageStats(myobj[[i]],plot=TRUE,both.strands=FALSE)
-        
+        dev.off() #pdf saved in current directory
     }
 }
 
-#obtaining nrow using loop
-for (i in 1:length(file.names.list)) {
-    print(nrow(myobj[[i]]))
-}
+#view where the pdf is located
+getwd()
 
-#descriptive statistics on the ERRBS, RRBS, WGBS, methylcaptureA, methylcaptureB, methylcaptureC
-#Option2
-for (i in 1:length(file.names.list)) {
-    print(basename(file.names.list[[i]]))
-    getMethylationStats(myobj[[i]])
-}
+#barplot of all samples 
+barplot(samples)
+#pdf(paste(barplot(samples), "barplot_samples.pdf", sep = "_"))
+#dev.off()
 
-#percent methylation distribution
-
-for (i in 1:length(file.names.list)) {
-    print(basename(file.names.list[[i]]))
-    getMethylationStats(myobj[[i]],plot=TRUE,both.strands=FALSE)
     
-}
-
-#Create a histogram with the percentage of CpG for each dataset
-for (i in 1:length(file.names.list)) {
-    print(basename(file.names.list[[i]]))
-    getCoverageStats(myobj[[i]],plot=TRUE,both.strands=FALSE)
-}
-
-
 #plot the samples
 plot(samples,
-          main = "DNA methylation Samples ",
-          xlab = "",
-          ylab = ""
-          
+main = "DNA methylation Samples ",
+xlab = "samples",
+ylab = "frequency",
+#dev.off()
 )
 
-#filtering the data by coverage
+
+#filtering the data by coverage to avoid bias (obtain coverage that is not too high or too low)
 filter_myobj = filterByCoverage(myobj,lo.count=10,lo.perc=NULL,
 hi.count=NULL,hi.perc=99.9)
 head(filter_myobj, n=20)
@@ -86,11 +93,10 @@ head(filter_myobj, n=20)
 meth=unite(myobj, destrand=FALSE)
 head(meth) #view the file meth
 
-# creates a methylBase object where only CpGs covered with at least 1 sample per group will be returned
+#creates a methylBase object where only CpGs covered with at least 1 sample per group will be returned
 meth.min=unite(myobj,min.per.group=1L)
 head(meth.min)
 
 #correlation
 getCorrelation(meth,plot=TRUE)
-
-#newdata
+#get numbers to compare
