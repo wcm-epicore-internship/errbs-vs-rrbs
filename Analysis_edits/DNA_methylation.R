026a8863f8853cd658a0e89548862c081c6517df
Thadeous Kacmarczyk <thk2008@med.cornell.edu>
2018-12-04 11:35:06 -0500

    saving edits and comments

--- Analysis_edits/DNA_methylation.R
+++ Analysis_edits/DNA_methylation.R
@@ -10,7 +10,7 @@ analysis.directory  <- "/Users/shazedaomar/Desktop/Internship_notes"
 working.directory   <- paste(analysis.directory, "MethylCall_Data_Plots", sep="/")
 data.file.directory <- paste(analysis.directory, "MethylCall_Data_Epicore", sep="/")
 
-# (thk2008) for me on my machine 
+# (thk2008) for me on my machine
 if (Sys.getenv()[["USER"]] == "thk2008") {
   analysis.directory  <- "/scratch001/thk2008/wcm-epicore-internship/errbs-vs-rrbs/Analysis_edits"
   working.directory   <- paste(analysis.directory, "MethylCall_Data_Plots", sep="/")
@@ -32,10 +32,10 @@ if (file.exists(working.directory)) {
 #methread for Multiple Files - load myobj.rda that is already created
 #if not myobj will be created
 
-# (thk2008) look for myobj in R environment, if it's already loaded, yay, do nothing otherwise
-# look for the saved data object and load it otherwise
-# create it from the raw methylcall data files
-if (!exists("myobj")) { 
+# (thk2008) if the data object myobj exists in the R environment, yay, it's already loaded
+# otherwise if the saved data object r data file exists in the current working directory, load it
+# otherwise create the data object myobj from the raw methylcall data files and save it
+if (!exists("myobj")) {
   cat("Did not find myobj\n")
   if (file.exists("myobj.rda")) {
     cat("myobj.rda loading\n")
@@ -50,7 +50,7 @@ if (!exists("myobj")) {
     }
     cat("creating and saving myobj\n")
     myobj <- methRead(as.list(file.names.list),
-      sample.id=as.list(gsub(".mincov0.txt.gz", "", basename(file.names.list))), #gsub finds all the files that end in ("*.mincov0.txt.gz"), 
+      sample.id=as.list(gsub(".mincov0.txt.gz", "", basename(file.names.list))), #gsub finds all the files that end in ("*.mincov0.txt.gz"),
       # Basename removes the ending of the file. -- are you sure? see ?basename
       assembly="hg19",
       # treatment=rep(0,6), #rep function - 0 = same -- (thk2008) what does 0 mean?.  6 = number of items in the file.names.list -- (thk2008) then use a variable or the length of the vector
@@ -122,7 +122,7 @@ if (!exists("meth")) {
 # load("/Users/shazedaomar/Desktop/Internship_notes/MethylCall_Data_Plots/meth.rda")
 # load("/Users/shazedaomar/Desktop/Internship_notes/MethylCall_Data_Plots/filter_myobj.rda")
 
-#Using perc.meth from the Methylation package to compare data 
+#Using perc.meth from the Methylation package to compare data
 perc.meth=percMethylation(meth)
 
 # (thk2008) this appears to be extra
@@ -130,7 +130,7 @@ perc.meth=percMethylation(meth)
 dens <- apply(perc.meth, 2, density)
 
 # (thk2008) this plot needs labeling. title, xaxis, yaxis
-# and eventually a discussion of what it is and means 
+# and eventually a discussion of what it is and means
 plot(NA, xlim=range(sapply(dens, "[", "x")), ylim=range(sapply(dens, "[", "y")))
 mapply(lines, dens, col=1:length(dens))
 legend("topright", legend=names(dens), fill=1:length(dens))
@@ -144,14 +144,14 @@ legend("topright", legend=names(dens), fill=1:length(dens))
 # and 22 cluster plots, and 22 PCA variance plots and 22 PCA plots
 # what is the purpose of break here?
 Analysis <- function(meth) {
-  
+
   for (i in 1:length(meth)) {
     getCorrelation(meth,plot=TRUE)
     clusterSamples(meth, dist="correlation", plot=TRUE) #find similarity in method
     #Principal component analysis - brings out variation and strong pattern in sample (helps to visualize/explore)
     PCASamples(meth, screeplot=TRUE)
     PCASamples(meth)
-    
+
     break
   }
 }
@@ -211,7 +211,7 @@ pdf(paste(plot(samples), "plot_samples.pdf", sep = "_"))
 # if (F) {
 # # (thk2008) this comment does match what the code is doing.
 # # what could it say?
-# #Store and output the sample.id 
+# #Store and output the sample.id
 # coverage_vals <- lapply(myobj, function(i){
 #   x <- getData(i)$coverage
 # })
@@ -238,13 +238,13 @@ boxplot(filt_coverage_vals)
 # (thk2008) the plots get overwritten
 # maybe consider dev.new() between plots or write them to a file
 plots <- function(filt_coverage_vals) {
-  
-  
+
+
   orig_margins <- par()$mar  # (thk2008) this needs to be done only once
   cl <- rainbow(6)           # (thk2008) save for this, it can be used for all the plots
 
   for (i in 1:length(filt_coverage_vals))  {
-    
+
     par(mar=c(7.1,4.1,4.1,2.1)) # (thk2008) changing the margins to see sample names
     # (thk2008) need to correct the labels
     plot(0, type="n", xlim=c(0,7), ylim=c(0,500), axes=F, main="Methylation analysis", xlab="Method", ylab="Coverage")
@@ -253,18 +253,18 @@ plots <- function(filt_coverage_vals) {
     }
     axis(2)
     axis(1, labels=names(filt_coverage_vals), at=seq(1,6), las=2)
-    
+
     plot(0, type="n", xlim=c(0,7), ylim=c(1,3), axes=F, main="all graphs must have labels", xlab="",ylab="")
     for (i in 1:length(filt_coverage_vals)) {
       vioplot(log10(filt_coverage_vals[[i]]), h=0.1, at=i, add=T, col=cl[i])
     }
-    
-    
+
+
     par(mfrow=c(3,2))
     for (i in 1:length(filt_coverage_vals)) {
       plot(density(filt_coverage_vals[[i]], bw=0.1), col=cl[i])
     }
-    
+
     par(mfrow=c(1,1))
     # (thk2008) careful, plotting filt_coverage_vals but legend has names from a different data object
     plot(0, type="n", xlim=c(0,500), ylim=c(0,0.21), axes=F, main="", xlab="",ylab="")
@@ -275,7 +275,7 @@ plots <- function(filt_coverage_vals) {
     }
     axis(1)
     axis(2)
-    
+
     # (thk2008) why the switch to coverage_vals from filt_coverage_vals?
     par(mfrow=c(3,2))
     for (i in 1:length(coverage_vals)){
@@ -283,8 +283,8 @@ plots <- function(filt_coverage_vals) {
     }
     axis(1) # (thk2008) does this do something?
     axis(2) # (thk2008) does this do something?
-    
-    
+
+
     par(mfrow=c(1,1))
     # (thk2008) careful, plotting filt_coverage_vals but legend has names from a different data object
     # also be careful cutting and pasting code. it will perpetuate the discrepancy or worse an error.
@@ -297,7 +297,7 @@ plots <- function(filt_coverage_vals) {
     }
     axis(2)
     axis(1)
-    
+
     break # (thk2008) what is the purpose break? does it do something?
   }
 }
